e62945a013c845280e6861ca9eebd79506f1721e
Peter Kos <pkos91@icloud.com>
2024-04-17 15:47:13 -0400

    Initial Commit

---
+++ PreviewAnimationStateBug/PreviewAnimationStateBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewAnimationStateBugApp.swift
+//  PreviewAnimationStateBug
+//
+//  Created by Peter Kos on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PreviewAnimationStateBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
